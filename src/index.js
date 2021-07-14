5152120fc49085026a2174095a03f22e558a8889
Prakash Ravichandran <prakash.ravichandran@hashouttech.com>
2021-07-14 14:09:39 +0530

    DAY -1 July 14 learnings

---
+++ src/index.js
@@ -0,0 +1,4 @@
+let packages = ['recharts','redux','react-bootstrap','babel'];
+let components = ['Image','CheckBox','Label','Buttons','modal','pop-over'];
+
+console.log(`Packages are - ${packages} , components are - ${components}`);
