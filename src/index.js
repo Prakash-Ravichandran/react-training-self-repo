e2bb331691df242bac276f283e27aaac42400680
Prakash Ravichandran <prakash.ravichandran@hashouttech.com>
2021-07-17 23:29:05 +0530

    added css loaders setup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
+import './styles/index.css';
+
 let packages = ['recharts','redux','react-bootstrap','babel'];
 let components = ['Image','CheckBox','Label','Buttons','modal','pop-over'];
 
 console.log(`Packages are - ${packages} , components are - ${components}`);
+
+let packCompo = [...packages,...components];
+console.log(packCompo);
+
+let countries = ['India','Australia','berma'];
+
+let newcountries = ['Bangladesh','Nepal'];
+
+let newarray = [...countries,...newcountries];
+
+console.log(newarray);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,30 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+
 module.exports = {
     mode : "development",
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+
+              {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+           
+        ]
+    },
+    plugins: [new MiniCssExtractPlugin()],
     devServer : {
         contentBase : "./dist"
-    }
+    },
+    devtool: 'source-map'
 }
 
